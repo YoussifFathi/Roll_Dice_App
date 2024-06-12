0d3ee42fd88c4e9b55c2c15b91754ab7b86a969d
YoussifFathi <yousefjoe1m@gmail.com>
2024-06-12 16:13:48 +0300

    create UI Home Screen

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,42 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+//class ViewController: UIViewController {
+//    @IBOutlet weak var diceImageOne: UIImageView!
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+//
+//
+//}
 
+class ViewController: UIViewController{
+    @IBOutlet weak var diceImageOne: UIImageView!
+    
+    @IBOutlet weak var diceImageTwo: UIImageView!
+    
+    var myNumbers = [ UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix"),]
+    var leftDiceNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageOne.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageTwo.image = UIImage(imageLiteralResourceName: "DiceSix")
     }
+    
+    @IBAction func RollButtonPressed(_ sender: UIButton) {
+        diceImageOne.image = myNumbers[leftDiceNumber]
+        
+        if(leftDiceNumber < 5){
+            leftDiceNumber+=1
 
+        }else{
+            leftDiceNumber = 0;
+        }
+        
 
+    }
+    
+    
 }
-
